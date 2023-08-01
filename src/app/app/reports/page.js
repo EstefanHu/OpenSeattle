dd402970178692229979050308e5913ccabbd7ea
Estefan Hu <estefanhu074@gmail.com>
2023-08-01 16:01:30 -0700

    dashboard => app

---
+++ src/app/app/reports/page.js
@@ -0,0 +1,9 @@
+import styles from './page.module.scss'
+
+export default function ReportsPage() {
+    return (
+        <div className={styles.reportsWrapper}>
+            <h1>Reports</h1>
+        </div>
+    )
+}
