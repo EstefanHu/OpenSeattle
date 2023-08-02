6d9e48a5d4508e043128c7d71eec366778fdc6a0
Estefan Hu <estefanhu074@gmail.com>
2023-08-02 12:02:50 -0700

    inventory report

--- src/app/app/reports/DonatorReport.js
+++ src/app/app/reports/DonatorReport.js
@@ -1,24 +1,27 @@
-'use client'
-import { useState, useEffect } from 'react'
+import prisma from '@/lib/prisma';
 import styles from './DonatorReport.module.scss'
 
-export default function DonatorReport() {
-    const [donators, setDonators] = useState({})
+const DEFAULT = { total: 0, food: 0, money: 0, clothes: 0, other: 0 }
 
-    useEffect(() => {
-        fetch('/app/reports/api', {
-            method: 'GET',
-        }).then(res => res.json())
-            .then(({ donators }) => {
-                setDonators(donators)
-            })
-    }, [])
+export default async function DonatorReport() {
+    // Prisma doesnt allow for complex SQL transactions so this is a temporary solution until I remove prisma
+    const donators = await prisma.donation.findMany({ include: { allocations: true } });
+
+    const res = {}
+
+    for (let i = 0; i < donators.length; i++) {
+        let d = donators[i]
+        const { name, type, value } = d
+        if (!res[name]) res[name] = structuredClone(DEFAULT)
+        res[name].total = res[name].total + 1
+        res[name][type] = res[name][type] + value
+    }
 
     return (
         <div className={styles.donatorReport}>
             <h2>Donator Report</h2>
 
-            {Object.entries(donators).map(([k, v]) => (
+            {Object.entries(res).map(([k, v]) => (
                 <div key={k}>
                     <p>{k}</p>
                     {Object.entries(v).map(([k, v]) => (
---
+++ src/app/app/reports/InventoryReport.js
@@ -0,0 +1,40 @@
+import prisma from '@/lib/prisma'
+import styles from './InventoryReport.module.scss'
+
+export default async function InventoryReport() {
+    const donations = await prisma.donation.findMany({
+        orderBy: { name: 'asc' },
+        include: { allocations: true }
+    })
+
+    return (
+        <div className={styles.inventoryReport}>
+            <h2>Inventory Report</h2>
+
+            <table>
+                <tr>
+                    <th>#</th>
+                    <th>Donor</th>
+                    <th>type</th>
+                    <th>Initial donation</th>
+                    <th># of allocations</th>
+                    <th>Allocations total</th>
+                </tr>
+
+                {donations.map(({ id, name, type, value, allocations }) => {
+
+                    return (
+                        <tr key={id}>
+                            <td>{id}</td>
+                            <td>{name}</td>
+                            <td>{type}</td>
+                            <td>{value}</td>
+                            <td>{allocations.length}</td>
+                            <td>{allocations.reduce((a, d) => a + d.amount, 0)}</td>
+                        </tr>
+                    )
+                })}
+            </table>
+        </div>
+    )
+}
